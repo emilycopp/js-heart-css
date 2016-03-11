c64a88d3d6a1346149beb958eee2b0661f48695a
emilycopp <emily.copp@bell.net>
2016-03-11 16:32:24 -0500

    sea slugs

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+var $firstP = $('main p:first-child');
+var $h2 = $('h2');
+var $mainList = $('main ul');
+var $img = $('img');
+var $liFirst = $('ul li:first-child');
+var $liLast = $('ul li:last-child');
+var $dlList = $('dl');
+var $dt = $('dl dt');
+
+$header.addClass('masthead');
+$h1.removeClass('big-heading');
+$h1.addClass('logo');
+$main.addClass('wrapper');
+$firstP.addClass('intro');
+$h2.addClass('slug-head');
+$mainList.addClass('slug-list');
+$img.addClass('slug-img');
+$liFirst.addClass('slug-list-first');
+$liLast.addClass('slug-list-last');
+$dlList.addClass('classification');
+$dt.removeClass('big-label');
+$liLast.show();
